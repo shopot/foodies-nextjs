70e6319aecea503849487beec7e77b4b9649c557
Sapronov Konstantin <ks.costas@gmail.com>
2024-07-27 11:58:01 +0300

    feat: extend rules for slugify

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,4 +3,10 @@ import xss from 'xss';
 
 export const filterXSS = (content: string) => xss(content);
 
-export const createSlug = (title: string) => slugify(title, { lower: true });
+export const createSlug = (title: string) =>
+  slugify(title, {
+    replacement: '_',
+    lower: true,
+    remove: /[*+~.()'"!:@]/g,
+    trim: true,
+  });
